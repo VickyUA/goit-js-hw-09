1248f5a2235c7c0fbb8365525572540a89bb268f
VickyUA <vickyy379@gmail.com>
2023-10-22 22:16:05 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyy = document.querySelector('body');
 
+let id;
+
+startBtn.addEventListener('click', () => {
+
+    id = setInterval(() => {
+        bodyy.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+
+    startBtn.disabled = true;
+}
+);
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(id);
+    startBtn.disabled = false;
+}
+);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
