a30b20ca6af716e7458460619d572182e8ad812e
VickyUA <vickyy379@gmail.com>
2023-10-25 23:41:11 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  const formInput = evt.currentTarget.elements;
+
+  let delay = Number(formInput.delay.value);
+  const step = Number(formInput.step.value);
+  const amount = Number(formInput.amount.value);  
+  
+    for (let i = 1; i <= amount; i +=1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;    
   }
 }
+
+function createPromise(position, delay) {
+  
+     return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const shouldResolve = Math.random() > 0.3;
+        if (shouldResolve) {
+          resolve({position, delay});
+        } else {
+          reject({position, delay});
+        };
+      }, delay);
+    });
+};
